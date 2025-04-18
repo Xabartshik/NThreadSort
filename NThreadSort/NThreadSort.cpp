661a9d7c605a3a0f9cb12f95622a79adac3e259f
Xabartshik <danoshlakov12@gmail.com>
2025-04-18 20:37:50 +0900

    Более эффективная работа с массивом благодаря использованию memcpy

--- NThreadSort/NThreadSort.cpp
+++ NThreadSort/NThreadSort.cpp
@@ -1,46 +1,84 @@
-﻿// main.cpp
-#include <iostream>
+﻿#include <iostream>
 #include <vector>
+#include <thread>
+#include <algorithm>
+#include <mutex>
 #include <random>
 #include <chrono>
 #include "utils.h"
+using namespace std;
+
+/*
+Введите количество потоков: 8
+Введите размер массива: 10000000
+Время сортировки: 370 мс
+0
+
+
+Введите количество потоков: 1
+Введите размер массива: 10000000
+Время сортировки: 1433 мс
+1
+*/
+void find_first_unsorted(const std::vector<int>& vec) {
+    for (size_t i = 1; i < vec.size(); ++i) {
+        if (vec[i] < vec[i - 1]) {
+            std::cout << "Found unsorted element: " << vec[i] << std::endl;
+            std::cout << "Index: " << i << std::endl;
+
+            if (i > 0) {
+                std::cout << "Left neighbors: " << vec[i - 1] << " " << vec[i - 2] << std::endl;
+            }
+            else {
+                std::cout << "No left neighbor (first element)" << std::endl;
+            }
+
+            if (i < vec.size() - 1) {
+                std::cout << "Right neighbors: " << vec[i + 1] << " " << vec[i + 2] << std::endl;
+            }
+            else {
+                std::cout << "No right neighbor (last element)" << std::endl;
+            }
+
+            return;
+        }
+    }
+
+    std::cout << "The vector is fully sorted in ascending order" << std::endl;
+}
 
 int main() {
-    int numThreads, numSubtasks, dataSize;
+    int numThreads = 8, dataSize;
 
-    std::cout << "Введите количество потоков: ";
-    std::cin >> numThreads;
+    cout << "Введите количество потоков: ";
+    cin >> numThreads;
 
-    std::cout << "Введите количество подзадач: ";
-    std::cin >> numSubtasks;
-
-    std::cout << "Введите размер массива: ";
-    std::cin >> dataSize;
+    cout << "Введите размер массива: ";
+    cin >> dataSize;
 
 
-    std::random_device rd;
-    std::mt19937 gen(rd());
-    std::uniform_int_distribution<> distrib(1, 1000); // Генерируем числа от 1 до 1000 (равномерное распределение)
+    random_device rd;
+    mt19937 gen(rd());
+    uniform_int_distribution<> distrib(1, dataSize-1); // Генерируем числа от 1 до 1000 (равномерное распределение)
 
-    std::vector<int> data(dataSize);
+    vector<int> data(dataSize);
     for (int i = 0; i < dataSize; ++i) {
         data[i] = distrib(gen);
     }
 
-    ParallelSorter sorter(numThreads, numSubtasks);
 
-    auto start = std::chrono::high_resolution_clock::now();
-    std::vector<int> sortedData = sorter.sort(data);
-    auto end = std::chrono::high_resolution_clock::now();
-    auto duration = std::chrono::duration_cast<std::chrono::milliseconds>(end - start);
+    auto start = chrono::high_resolution_clock::now();
+    parallel_sort(data, numThreads);
+    auto end = chrono::high_resolution_clock::now();
+    auto duration = chrono::duration_cast<chrono::milliseconds>(end - start);
 
-    std::cout << "Время сортировки: " << duration.count() << " мс" << std::endl;
+    cout << "Время сортировки: " << duration.count() << " мс" << endl;
+
+    find_first_unsorted(data);
 
     // Вывод отсортированного массива (для проверки)
-    for (int x : sortedData) {
-        std::cout << x << " ";
-    }
-    std::cout << std::endl;
+    cout << is_sorted(data.cbegin(), data.cend()) << " ";
+    cout << endl;
 
     return 0;
 }
--- NThreadSort/utils.h
+++ NThreadSort/utils.h
@@ -1,124 +1,73 @@
 #include <vector>
 #include <thread>
-#include <mutex>
 #include <algorithm>
-#include <random>
+#include <cstring>
 
-class ParallelSorter {
-public:
-    // Конструктор: инициализирует количество потоков и подзадач
-    ParallelSorter(int numThreads, int numSubtasks) : numThreads_(numThreads), numSubtasks_(numSubtasks) {}
+// Функция для сортировки части массива
+void sort_part(std::vector<int>& arr, int start, int end) {
+    std::sort(arr.begin() + start, arr.begin() + end);
+}
 
-    // Основной метод сортировки
-    std::vector<int> sort(std::vector<int>& data) {
-        // Копируем входные данные в член класса для доступа из других методов
-        data_ = data;
-        // Сохраняем размер данных
-        size_ = data_.size();
-        // Вычисляем размер каждой подзадачи
-        subtaskSize_ = size_ / numSubtasks_;
+// Функция для слияния двух отсортированных частей массива
+void merge(std::vector<int>& arr, int start1, int end1, int start2, int end2) {
+    std::vector<int> temp(end2 - start1);
 
-
-        // Создаем и запускаем потоки
-        threads_.reserve(numThreads_);
-        for (int i = 0; i < numThreads_; ++i) {
-            // Каждый поток выполняет метод sortSubtasks
-            threads_.emplace_back(&ParallelSorter::sortSubtasks, this, i);
+    int i = start1, j = start2, k = 0;
+    while (i < end1 && j < end2) {
+        if (arr[i] <= arr[j]) {
+            temp[k++] = arr[i++];
         }
-
-        // Ожидаем завершения всех потоков
-        for (auto& thread : threads_) {
-            thread.join();
-        }
-
-        // Сливаем отсортированные подзадачи в один результирующий массив
-        mergeSubtasks();
-
-        // Возвращаем отсортированный массив
-        return data_;
-    }
-
-
-private:
-    // Метод, выполняемый каждым потоком
-    void sortSubtasks(int threadId) {
-        // Вычисляем начальный и конечный индексы подзадач для текущего потока
-        int startTask = threadId * (numSubtasks_ / numThreads_);
-        int endTask = (threadId + 1) * (numSubtasks_ / numThreads_);
-        // Корректируем конец для последнего потока, чтобы он обработал все оставшиеся подзадачи
-        if (threadId == numThreads_ - 1) {
-            endTask = numSubtasks_;
-        }
-
-        // Сортируем каждую подзадачу, назначенную текущему потоку
-        for (int i = startTask; i < endTask; ++i) {
-            int start = i * subtaskSize_;
-            int end = (i + 1) * subtaskSize_;
-            // Корректируем конец для последней подзадачи, чтобы обработать все оставшиеся элементы
-            if (i == numSubtasks_ - 1) {
-                end = size_;
-            }
-            // Используем std::sort для сортировки подмассива
-            std::sort(data_.begin() + start, data_.begin() + end);
+        else {
+            temp[k++] = arr[j++];
         }
     }
 
-    // Метод для слияния отсортированных подзадач
-    void mergeSubtasks() {
-        // Создаем результирующий массив
-        std::vector<int> merged;
-        merged.reserve(size_);
-
-        // Массив индексов для отслеживания текущей позиции в каждой подзадаче
-        std::vector<int> indices(numSubtasks_, 0);
-
-        // Проходим по всем элементам исходного массива
-        for (int i = 0; i < size_; ++i) {
-            // Находим минимальный элемент среди текущих элементов всех подзадач
-            int minVal = INT_MAX;
-            int minIndex = -1;
-            int subtaskIndex = -1;
-
-
-            for (int j = 0; j < numSubtasks_; ++j) {
-                int start = j * subtaskSize_;
-                int end = (j + 1) * subtaskSize_;
-                if (j == numSubtasks_ - 1) {
-                    end = size_;
-                }
-                // Проверяем, не достигли ли конца текущей подзадачи
-                if (indices[j] < end - start) {
-                    int currentVal = data_[start + indices[j]];
-                    if (currentVal < minVal) {
-                        minVal = currentVal;
-                        minIndex = indices[j];
-                        subtaskIndex = j;
-                    }
-                }
-            }
-
-            // Добавляем минимальный элемент в результирующий массив
-            if (subtaskIndex != -1) {
-                merged.push_back(minVal);
-                indices[subtaskIndex]++; // Переходим к следующему элементу в выбранной подзадаче
-            }
-        }
-
-        // Заменяем исходный массив отсортированным
-        data_ = merged;
+    // Копируем оставшиеся элементы из левой части
+    while (i < end1) {
+        temp[k++] = arr[i++];
     }
 
+    // Копируем оставшиеся элементы из правой части
+    while (j < end2) {
+        temp[k++] = arr[j++];
+    }
 
-    // Количество потоков
-    int numThreads_;
-    // Количество подзадач
-    int numSubtasks_;
-    // Массив данных для сортировки
-    std::vector<int> data_;
-    // Размер массива данных
-    size_t size_;
-    // Размер каждой подзадачи
-    int subtaskSize_;
-    // Вектор потоков
-    std::vector<std::thread> threads_;
-};
+    // Копируем обратно в исходный массив
+    std::memcpy(&arr[start1], &temp[0], temp.size() * sizeof(int));
+}
+
+void parallel_sort(std::vector<int>& arr, int num_threads) {
+    if (arr.empty()) return;
+
+    int size = arr.size();
+    num_threads = std::min(num_threads, size);
+    int chunk_size = (size + num_threads - 1) / num_threads;  // Округление вверх
+
+    std::vector<std::thread> threads;
+
+    // Сортировка частей массива в отдельных потоках
+    for (int i = 0; i < num_threads; ++i) {
+        int start = i * chunk_size;
+        int end = std::min(start + chunk_size, size);
+        /*
+        Типа push_back, но вызывает внутри себя конструктор, создава объект на основе переданных параметров
+        */
+        threads.emplace_back(sort_part, std::ref(arr), start, end);
+    }
+
+    // Ожидание завершения всех потоков
+    for (auto& thread : threads) {
+        thread.join();
+    }
+
+    // Последовательное слияние отсортированных частей
+    for (int merge_size = chunk_size; merge_size < size; merge_size *= 2) {
+        for (int left = 0; left < size; left += 2 * merge_size) {
+            int mid = std::min(left + merge_size, size);
+            int right = std::min(left + 2 * merge_size, size);
+            if (mid < right) {
+                merge(arr, left, mid, mid, right);
+            }
+        }
+    }
+}
